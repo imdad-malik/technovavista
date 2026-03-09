d531dd2daf7b91348e7e1cb1299b2023c41a0c16
Imdad Ali Malik <your-email@example.com>
2026-03-09 19:58:52 +0500

    fix turbopack worker error

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,21 @@
 // next.config.mjs
 
-import remarkGfm from 'remark-gfm'
-import rehypeRaw from 'rehype-raw'
+import remarkGfm from "remark-gfm";
+import rehypeRaw from "rehype-raw";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    mdxRs: true, // optional: if you're using .mdx, enable this
+    mdxRs: true,
   },
-  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+
+  pageExtensions: ["js", "jsx", "md", "mdx"],
+
+  turbopack: {},
+
   webpack(config, options) {
     return config;
   },
-  // Optional: expose plugins to wherever you're parsing MD (e.g., contentlayer or your markdown loader)
-  // This part is needed only if you're not using next-mdx-remote or MDX plugin
 };
 
 export default nextConfig;
