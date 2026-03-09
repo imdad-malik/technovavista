feefae64ef47c6861288db14e75912bc47676934
Imdad Ali Malik <your-email@example.com>
2026-03-09 19:28:35 +0500

    update sitemap and robots for vercel domain

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 // next-sitemap.config.js
 
 module.exports = {
-  siteUrl: 'https://article.technovavista.com',
+  siteUrl: 'https://technovavista.vercel.app',
   generateRobotsTxt: true,
   sitemapSize: 5000,
   robotsTxtOptions: {
